33b608208072ef824074d88ed86c64e287904d34
alexey <alexnagornnyy.an@gmail.com>
2024-04-08 21:00:52 +0100

    pub fn get_symbol_hash

--- src/token_utils.rs
+++ src/token_utils.rs
@@ -146,7 +146,7 @@ pub async fn get_token_contract(wallet: &WalletUnlocked) -> TokenContract<Wallet
 //     (assets, asset_configs)
 // }
 
-fn get_symbol_hash(symbol: &str) -> Bits256 {
+pub fn get_symbol_hash(symbol: &str) -> Bits256 {
     let mut hasher = Sha256::new();
     hasher.update(symbol);
     let symbol_hash: [u8; 32] = hasher.finalize().into();
